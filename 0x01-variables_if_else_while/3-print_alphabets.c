2b95024dd2e6215f9ecc4016f01fb0177a5dc008
keysmusician <justin.masayda@gmail.com>
2021-02-04 20:24:12 +0000

    Add task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for alphabet printing function
+ * Return: 0-status code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
