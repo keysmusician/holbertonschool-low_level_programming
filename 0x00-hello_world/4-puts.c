a08efe3e5ce1fcca9e478d3bbb568aad53c87891
keysmusician <justin.masayda@gmail.com>
2021-02-04 00:40:48 +0000

    Update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a quote fragment.
+ * Return: returns an int (termination code)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
