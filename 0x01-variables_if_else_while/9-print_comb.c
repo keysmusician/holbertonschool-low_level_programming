33139cc3f86df0104d16e0b1068e341bc8570a56
keysmusician <justin.masayda@gmail.com>
2021-02-04 22:32:54 +0000

    Update task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,27 +7,20 @@
 int main(void)
 {
 	int num1;
-	int num2;
 
 	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (num2 = '0'; num2 <= '9'; num2++)
+		putchar(num1);
+		if (num1 == '9')
 		{
-			putchar(num1);
-			putchar(num2);
-			if (num1 == '9' && num2 == '9')
-			{
-				continue;
-			}
-			
-			else 
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			continue;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
 	putchar(10);
 
 	return (0);
