d1a9eb12b2ad3ef2de0089219f5648c028226073
keysmusician <justin.masayda@gmail.com>
2021-02-04 20:47:56 +0000

    Add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for alphabet printing function
+ * Return: 0-status code
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar(10);
+
+	return (0);
+}
