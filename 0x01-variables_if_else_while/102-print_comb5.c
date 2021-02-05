9190dc5590282d054e44b48949b20bd258f20049
keysmusician <justin.masayda@gmail.com>
2021-02-05 21:59:58 +0000

    Update task 13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,32 +12,35 @@ int main(void)
 	{
 		for (num2 = '0'; num2 <= '9'; num2++)
 		{
-			for (num3 = (num2 == '9') ? num1 + 1 : num1;
-			     num3 <= '9'; num3++)
-			{
-				for (num4 = (num2 == '9') ? num2 + 1 : '0';
-				      num4 <= '9'; num4++)
-				{
-					{
-					putchar(num1);
-					putchar(num2);
-					putchar(' ');
-					putchar(num3);
-					putchar(num4);
-					if (num1 == '9' && num2 == '8'
-					    && num3 == '9' &&
-					    num4 == '9')
-					{
-						goto end;
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
-				}
-			}
+		if (num2 == '9')
+		{
+			num4 = '0';
+			num3 = num1 + 1;
+		}
+		else
+		{
+			num4 = num2 + 1;
+			num3 = num1;
+		}
+		for (; num3 <= '9'; num3++)
+		{
+		for (; num4 <= '9'; num4++)
+		{
+		putchar(num1);
+		putchar(num2);
+		putchar(' ');
+		putchar(num3);
+		putchar(num4);
+		if (num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9')
+			goto end;
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+		num4 = '0';
+		}
 		}
 	}
 end:
