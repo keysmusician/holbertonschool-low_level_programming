5a04b95c2b75685c7740c184d9070f0fb2cf0387
keysmusician <justin.masayda@gmail.com>
2021-02-05 19:16:56 +0000

    Update task 13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,12 +6,7 @@
  */
 int main(void)
 {
-	/*set a*/
-	int num1;
-	int num2;
-	/*set b*/
-	int num3;
-	int num4;
+	int num1, num2, num3, num4;
 	int flip = 0;
 
 	for (num1 = '0'; num1 <= '9'; num1++)
@@ -20,30 +15,35 @@ int main(void)
 		{
 			for (num3 = num1; num3 <= '9'; num3++)
 			{
-				num4 = flip ? num2 + 1 : '0';
+				num4 = (flip && num2 != '9') ? num2 + 1 : '0';
 				for (; num4 <= '9'; num4++)
 				{
 					flip = 0;
+					if (num3 >= num1)
+					{
 					putchar(num1);
 					putchar(num2);
 					putchar(' ');
 					putchar(num3);
 					putchar(num4);
-					if (num1 == '9' && num2 == '8' &&
-					    num3 == '9' && num4 == '9')
+					if (num1 == '9' && num2 == '8'
+					    && num3 == '9' &&
+					    num4 == '9')
 					{
-						break;
+						goto end;
 					}
 					else
 					{
 						putchar(',');
 						putchar(' ');
 					}
+					}
 				}
 			}
 			flip = 1;
 		}
 	}
+end:
 	putchar(10);
 	return (0);
 }
