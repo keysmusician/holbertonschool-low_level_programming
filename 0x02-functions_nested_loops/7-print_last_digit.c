af0324e390aa1fbf32a44cb523ffb292b7584b4e
keysmusician <justin.masayda@gmail.com>
2021-02-09 19:29:30 +0000

    Add space

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+
 /**
  * print_last_digit - prints the last digit of a number
  * @n: number to check the last digit of
  * Return: Value of the last digit of input
  */
+
 int print_last_digit(int n)
 {
 	int last_digit = (n >= 0) ? (n % 10) : (-n % 10);
