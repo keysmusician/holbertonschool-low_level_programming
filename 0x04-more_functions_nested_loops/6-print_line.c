39b5083d2fabffe5bae8f6f72321d549c6ed26ed
keysmusician <justin.masayda@gmail.com>
2021-02-11 19:10:17 +0000

    Add condition if n < 1, rename function

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,24 @@
 #include "holberton.h"
+
 /**
  * print_line - Draws a straight line in the terminal using '_'
  * @n: Number of '_' (underscore) to draw
  * Return: void
  */
-void print_numbers(int n)
+void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n < 1)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
