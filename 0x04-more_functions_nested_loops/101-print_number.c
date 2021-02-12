7bbad00f477b2987a8f011dde8d2e0cae0a55422
keysmusician <justin.masayda@gmail.com>
2021-02-12 18:51:17 +0000

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints int n one char at a time
+ * @n: Number to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	int digits = 1;
+	int power = 1;
+	int m = n;
+	int exponent = 0;
+
+/*ensure n is positive from this point on*/
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+/*count the number of digits*/
+	while (m / 10)
+	{
+		digits++;
+		m = m / 10;
+	}
+
+/*Print each digit one at a time*/
+	for (; digits > 0; digits--)
+	{
+		power = 1;
+		for (exponent = 1; exponent < digits; exponent++)
+		{
+			power *= 10;
+		}
+		_putchar('0' + (n / power) % 10);
+	}
+	_putchar('\n');
+}
