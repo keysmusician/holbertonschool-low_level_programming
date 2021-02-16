a11e9671826aa87e235524cfd0ed728838b5a35f
keysmusician <justin.masayda@gmail.com>
2021-02-16 19:52:23 +0000

    len -1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,7 +21,7 @@ void puts_half(char *str)
 	len = (end - start);
 
 /*print from half length to end of string*/
-	for (i = len / 2; i < len; i++)
+	for (i = (len - 1) / 2; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
