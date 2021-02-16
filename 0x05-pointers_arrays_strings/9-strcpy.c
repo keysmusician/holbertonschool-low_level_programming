7c6a213f59c761975f316779a7098bed7be45a0e
keysmusician <justin.masayda@gmail.com>
2021-02-16 18:43:41 +0000

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest : destination buffer
+ * @src : source string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *end = src;
+	int i, len;
+
+	while (end != '\0')
+	{
+		end++;
+	}
+
+	len = end - src;
+
+	printf("len = %d\n", len);
+
+	for (i = 0; i <= len; i++)
+	{
+		printf("Itteration: %d\n", i);
+		dest[i] = src[i];
+	}
+	return (dest);
+}
