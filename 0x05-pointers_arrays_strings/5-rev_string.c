e8433f2ace94fbc245cbdd9caabb30d68156c77e
keysmusician <justin.masayda@gmail.com>
2021-02-17 14:25:59 +0000

    Change loop condition from > to >=

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@ void rev_string(char *s)
 {
 	int index = 0;
 	int length = 0;
-	char buffer = '\0';
+	char buffer = 0;
 
 /*find length of string including Null*/
 	while (s[length] != '\0')
@@ -17,16 +17,30 @@ void rev_string(char *s)
 		length++;
 	}
 
-/*Exclude NULL from length*/
 	if (length)
 	{
+
+/*Exclude NULL from length*/
 		length--;
 
-		for (index = length; index > (length - length / 2); index--)
+		for (index = length; index >= (length - length / 2); index--)
 		{
 			buffer = s[index];
 			s[index] = s[length - index];
 			s[length - index] = buffer;
 		}
 	}
+}
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	char s[11] = "Holberton!";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
 }
