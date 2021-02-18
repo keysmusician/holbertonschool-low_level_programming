1f87365e413338a34215447baf0d6bf14af102b0
keysmusician <justin.masayda@gmail.com>
2021-02-18 20:24:20 +0000

    Calc difference between first unequal chars

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 		i++;
 
-	if (s1[i] > s2[i])
-		compare = 15;
-	else if (s1[i] < s2[i])
-		compare = -15;
+	compare = s1[i] - s2[i];
 
 	return (compare);
 }
