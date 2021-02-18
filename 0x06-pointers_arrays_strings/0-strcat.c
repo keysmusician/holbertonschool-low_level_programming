d8502887f119b11b9d12577cd3ea18cf7e0148cf
keysmusician <justin.masayda@gmail.com>
2021-02-18 17:59:11 +0000

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest : string to be appended to
+ * @src : string to append
+ * Return: concatenation of strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_end = 0;
+	int i = 0;
+
+	while (dest[dest_end] != '\0')
+		dest_end++;
+
+	while (src[i] != '\0')
+	{
+		dest[dest_end + i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
