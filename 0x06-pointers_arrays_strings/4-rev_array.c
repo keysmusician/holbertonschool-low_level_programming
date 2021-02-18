876329099ce3ce23ddca1f7967af91b3937becdc
keysmusician <justin.masayda@gmail.com>
2021-02-18 21:58:35 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a : array
+ * @n : array length
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int buffer, i = 0;
+
+	while (i <= n/2)
+	{
+		buffer = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = buffer;
+		i++;
+	}
+}
