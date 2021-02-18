411c857fd8b3d6e7361eb077cd670975bc387788
keysmusician <justin.masayda@gmail.com>
2021-02-18 22:08:51 +0000

    Change loop condition <= to <

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,11 +8,11 @@ void reverse_array(int *a, int n)
 {
 	int buffer, i = 0;
 
-	while (i <= n / 2)
+	while (i < n / 2)
 	{
 		buffer = a[i];
-		a[i] = a[n - i];
-		a[n - i] = buffer;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = buffer;
 		i++;
 	}
 }
