7100392d199e5c54880d4f329c4ac78e13cef636
keysmusician <justin.masayda@gmail.com>
2021-02-19 18:37:58 +0000

    Task 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,21 +6,20 @@
 char *rot13(char *s)
 {
 	int x, y;
-	char alpha[100] = "NMOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ\
-nmopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+	char alpha[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 /* Map chars from ASCII charset to ROT13 charset */
 	for (x = 0; s[x] != '\0'; x++)
 	{
 		for (y = 0; alpha[y] != '\0'; y++)
 		{
-			if (alpha[y] == s[x])
+			if (s[x] == alpha[y])
 			{
-				s[x] = alpha[y + 13];
+				s[x] = rot13[y];
 				break;
 			}
 		}
 	}
-
 	return (s);
 }
