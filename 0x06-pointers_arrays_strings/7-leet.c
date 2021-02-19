f05443006c2b483467cd5c5c6c62b92a360e16bf
keysmusician <justin.masayda@gmail.com>
2021-02-19 15:46:02 +0000

    Fix wrong ASCII char value

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,11 +12,11 @@ char *leet(char *s)
 	for (; i < 128; i++)
 		leet[i] = '\0' + i;
 
-/* Overwrite leet/1337 special characters */
+/* Overwrite ASCII chars with corresponding leet/1337 special characters */
 	leet[97] = '4';  /* 'a' */
 	leet[65] = '4';  /* 'A' */
 	leet[101] = '3'; /* 'e' */
-	leet[45] = '3';  /* 'E' */
+	leet[69] = '3';  /* 'E' */
 	leet[108] = '1'; /* 'l' */
 	leet[76] = '1';  /* 'L' */
 	leet[111] = '0'; /* 'o' */
@@ -24,7 +24,7 @@ char *leet(char *s)
 	leet[116] = '7'; /* 't' */
 	leet[84] = '7';  /* 'T' */
 
-/* Replace ASCII charset with leet/1337 charset */
+/* Map chars from ASCII charset to leet/1337 charset */
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		li = s[i];
