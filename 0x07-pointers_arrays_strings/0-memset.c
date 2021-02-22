39383d6987766d58b6ad8e751a24488f94ab3814
keysmusician <justin.masayda@gmail.com>
2021-02-22 18:07:53 +0000

    Update task 0& header

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
 /**
  * _memset - fills n bytes of memory with a constant byte
  * @s : Pointer to memory area to begin writing to
  * @b : Constant byte to fill memory with
  * @n : Number of bytes to write
- * Return: 0-status code
+ * Return: Location of beginning of memory write (s)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
