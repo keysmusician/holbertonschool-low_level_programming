53ececd5149b8699aac298c6756e609276d1c4df
keysmusician <justin.masayda@gmail.com>
2021-02-22 22:42:32 +0000

    Task 8 (file #9)

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * set_string - Sets the value of a pointer-to-a-char (char*)
+ * @s : P
+ * @to : Size of the square matricies
+ * Return: Void;
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
