aec0bc82135a0b888cf40446ab76c5e6dcd61bd5
keysmusician <justin.masayda@gmail.com>
2021-02-24 21:21:09 +0000

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+/**
+ * calculate_root - Compares n to various square numbers
+ * @n : Number to find root of
+ * @root : Then natural square root of n if it exits
+ * Return : Root if exists, -1 if not
+ */
+int calculate_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+	else if (root * root == n)
+		return (root);
+
+	return help(n, root + 1);
+}
+
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n : Number
+ * Return: Natural square root of n, or -1 if it doesn't exist
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return calculate_root(n, 1);
+}
