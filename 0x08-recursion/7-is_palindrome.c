2de717e7373f40ead8f986bbc1f135f1c002e724
keysmusician <justin.masayda@gmail.com>
2021-02-24 22:45:05 +0000

    Task 7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,36 @@
+/**
+ * get_end - Finds the end of a string
+ * @s: String
+ * Return: Pointer to end of string
+ */
+char* get_end(char *s)
+{
+	if (*s == '\0')
+		return(s - 1);
+
+	return (get_end(s + 1));
+}
+
+int compare(char *a, char *b)
+{
+	if (a)
+	{
+		if (*a == *b)
+			compare(a + 1, b - 1);
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: String
+ * Return: 1 if the string is a palindrome, otherwise return 0
+ */
+int is_palindrome(char *s)
+{
+	char *end = get_end(s);
+
+	return compare(s, end);
+}
