450fdde37411ef612a08c0f4943bba5f45cbab4a
keysmusician <justin.masayda@gmail.com>
2021-02-26 20:30:57 +0000

    Add task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into it
+ * @argc: Number of input arguments (minimum 1)
+ * @argv: Array of string arguments
+ * Return: 0 (success) status code
+ */
+int main(int argc, char *argv[])
+{
+/*
+ * Do something useless with argv to suppress compiler warning
+ */
+	argv += 0;
+
+	printf("%i\n", argc);
+
+	return (0);
+}
