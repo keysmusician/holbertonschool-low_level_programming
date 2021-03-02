92a499681fd374e5e232aacd175c517a6d8e252e
keysmusician <justin.masayda@gmail.com>
2021-03-02 17:48:26 +0000

    Return NULL if size == 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,10 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc(size);
 	unsigned int i;
 
-	if (array != NULL && size != 0)
+	if (!size)
+		array = NULL;
+
+	if (array != NULL)
 	{
 		for (i = 0; i < size; i++)
 			array[i] = c;
