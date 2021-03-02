a8bb9dd94d45b7b3377cb0a3af6ab5cd0bc540c3
keysmusician <justin.masayda@gmail.com>
2021-03-02 22:42:49 +0000

    Handle memory leaks

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -8,42 +8,42 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *cat;
-        unsigned int s1_length = 0;
-        unsigned int s2_length = 0;
-        unsigned int i, total_length = 0;
+	char *cat;
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
+	unsigned int i, total_length = 0;
 
-        if (s1 != NULL)
-        {
-                while (s1[s1_length++])
-                        continue;
-        }
-        else
-        {
-                s1_length = 1;
-                s1 = "";
-        }
-        if (s2 != NULL)
-        {
-                while (s2[s2_length++])
-                        continue;
-        }
-        else
-        {
-                s2_length = 1;
-                s2 = "";
-        }
+	if (s1 != NULL)
+	{
+		while (s1[s1_length++])
+			continue;
+	}
+	else
+	{
+		s1_length = 1;
+		s1 = "";
+	}
+	if (s2 != NULL)
+	{
+		while (s2[s2_length++])
+			continue;
+	}
+	else
+	{
+		s2_length = 1;
+		s2 = "";
+	}
 	/* Storage for both null bytes is not required, hence -1 */
-        total_length = s1_length + s2_length - 1;
-        cat = malloc(total_length);
-        if (cat != NULL)
-        {
-                for (i = 0; i < s1_length; i++)
-                        cat[i] = s1[i];
-                for (i = 0; i < s2_length; i++)
-                        cat[i + s1_length - 1] = s2[i];
-        }
-        return (cat);
+	total_length = s1_length + s2_length - 1;
+	cat = malloc(total_length);
+	if (cat != NULL)
+	{
+		for (i = 0; i < s1_length; i++)
+			cat[i] = s1[i];
+		for (i = 0; i < s2_length; i++)
+			cat[i + s1_length - 1] = s2[i];
+	}
+	return (cat);
 }
 
 /**
@@ -55,15 +55,18 @@ char *str_concat(char *s1, char *s2)
 char *argstostr(int ac, char **av)
 {
 	int argument;
-	char *cat = "";
+	char *cat;
+	char *temp = NULL;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (argument = 0; argument < ac; argument++)
 	{
-		cat = str_concat(cat, av[argument]);
-		cat = str_concat(cat, "\n");
+		temp = str_concat(temp, av[argument]);
+		cat = str_concat(temp, "\n");
+		free(temp);
+		temp = cat;
 	}
 
 	return (cat);
