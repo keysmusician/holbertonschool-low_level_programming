f1fa63fbf96aa146233664652a5fda34bb1dc201
keysmusician <justin.masayda@gmail.com>
2021-03-02 22:23:45 +0000

    Insert concat function definition

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,5 +1,50 @@
-#include "holberton.h"
 #include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings into newly allocated memory
+ * @s1: String 1
+ * @s2: String 2
+ * Return: Pointer to the new array, or NULL if it fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+        char *cat;
+        unsigned int s1_length = 0;
+        unsigned int s2_length = 0;
+        unsigned int i, total_length = 0;
+
+        if (s1 != NULL)
+        {
+                while (s1[s1_length++])
+                        continue;
+        }
+        else
+        {
+                s1_length = 1;
+                s1 = "";
+        }
+        if (s2 != NULL)
+        {
+                while (s2[s2_length++])
+                        continue;
+        }
+        else
+        {
+                s2_length = 1;
+                s2 = "";
+        }
+	/* Storage for both null bytes is not required, hence -1 */
+        total_length = s1_length + s2_length - 1;
+        cat = malloc(total_length);
+        if (cat != NULL)
+        {
+                for (i = 0; i < s1_length; i++)
+                        cat[i] = s1[i];
+                for (i = 0; i < s2_length; i++)
+                        cat[i + s1_length - 1] = s2[i];
+        }
+        return (cat);
+}
 
 /**
  * argstostr - Concatenates all the arguments of your program
