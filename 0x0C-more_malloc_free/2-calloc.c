33e7a585a29edd58e7729e68c55b2486f7d5042d
keysmusician <justin.masayda@gmail.com>
2021-03-04 22:42:18 +0000

    Add task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates zero-initialized memory for an array
+ * @nmemb: Number of members to allocate space for
+ * @size: Size of each member
+ * Return: Void pointer to allocated memory, 98 on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *memory = NULL;
+	unsigned int i;
+
+	if (!nmemb || !size)
+		return (NULL);
+
+	memory = malloc(nmemb * size);
+	if (memory == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		memory[i] = 0;
+
+	return (memory);
+}
