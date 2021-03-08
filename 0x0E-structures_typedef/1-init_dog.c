f8ab927644b9155b5f2a73a00074509586ad59ab
keysmusician <justin.masayda@gmail.com>
2021-03-08 21:23:49 +0000

    Add protoype to header, Add task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to a struct dog
+ * @name: Name to assign
+ * @age: Age to assign
+ * @owner: Owner to assign
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
