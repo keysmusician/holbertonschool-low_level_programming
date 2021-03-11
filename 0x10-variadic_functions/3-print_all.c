39bc32b8595ef38b28d54ef78405108e7c9fbfd7
keysmusician <justin.masayda@gmail.com>
2021-03-11 20:39:03 +0000

    Add condition for delimiting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,6 +10,7 @@ void print_all(const char * const format, ...)
 {
 	va_list arguments;
 	char *string;
+	int delimit = 0;
 	unsigned int i = 0;
 	char f;
 
@@ -19,15 +20,19 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
+			delimit = 1;
 			printf("%c", va_arg(arguments, int));
 			break;
 		case 'i':
+			delimit = 1;
 			printf("%d", va_arg(arguments, int));
 			break;
 		case 'f':
+			delimit = 1;
 			printf("%f", va_arg(arguments, double));
 			break;
 		case 's':
+			delimit = 1;
 			string = va_arg(arguments, char *);
 			if (!string)
 			{
@@ -38,7 +43,7 @@ void print_all(const char * const format, ...)
 		}
 		i++;
 		f = format[i];
-		if (f == 'c' || f == 'i' || f == 'f' || f == 's')
+		if (delimit && (f == 'c' || f == 'i' || f == 'f' || f == 's'))
 			printf(", ");
 	}
 	printf("\n");
