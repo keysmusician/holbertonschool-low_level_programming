f80219e6961c1cb87b49ed4068dccd5f6800b76b
keysmusician <justin.masayda@gmail.com>
2021-03-11 01:03:14 +0000

    Add task 4 (advanced)

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints its own opcodes
+ * @argc: Number of arguments
+ * @argv: List of arguments
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int bytes, i;
+	char *main_p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	main_p = main;
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+		printf("%x ", main_p[i]);
+	printf("\n");
+	return (0);
+}
