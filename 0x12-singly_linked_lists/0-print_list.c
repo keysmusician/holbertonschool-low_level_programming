41feee4a7f73074f62c1cd432ffad2773b42b70e
keysmusician <justin.masayda@gmail.com>
2021-03-18 19:40:44 +0000

    Add task 0, header, readme

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: Head node of list to print
+ * Return: The number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	size_t node_count = 0;
+
+	if (!h)
+		return (0);
+
+	while (current)
+	{
+		if (!current->str)
+
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", current->len, current->str);
+		current = current->next;
+		node_count++;
+	}
+	return (node_count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+0x12. C - Singly linked lists
+At the end of this project, you are expected to be able to explain to anyone:
+When and why to use linked lists vs arrays
+How to build and use linked lists
