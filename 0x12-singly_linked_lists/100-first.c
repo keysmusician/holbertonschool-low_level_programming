95e5eb8ab745326ab862d72bc9910495938df94c
keysmusician <justin.masayda@gmail.com>
2021-03-18 23:19:49 +0000

    Format edits

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,9 @@
 /**
  * myStartupFun - Prints a line from "The Hare and the Tortoise"
  */
-void myStartupFun (void) __attribute__ ((constructor));
-void myStartupFun (void)
+void myStartupFun(void) __attribute__ ((constructor));
+void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n'");
+	printf("You're beat! and yet, you must allow,\nI bore my house \
+upon my back!\n");
 }
