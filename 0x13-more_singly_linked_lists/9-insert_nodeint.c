82f806bcbe09070521bb5e02f6b65882b2b8fdfb
keysmusician <justin.masayda@gmail.com>
2021-03-23 15:14:30 +0000

    Update head value if new node is at index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	if (idx == 0)
+	{
 		new->next = *head;
+		*head = new;
+	}
 	else
 	{
 		new->next = prev_node->next;
