0e235ac1688337a4ed906572f3386c36c4cf3c0c
keysmusician <justin.masayda@gmail.com>
2021-03-25 20:40:32 +0000

    Add condition to print '0' if n == 0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,15 @@
 void print_binary(unsigned long int n)
 {
 	char c = (1 & n) + '0';
+	static int first_call = 1;
 
+	if (n == 0 && first_call)
+	{
+		_putchar('0');
+		return;
+	}
+
+	first_call = 0;
 	if (n > 0)
 	{
 		print_binary(n >> 1);
