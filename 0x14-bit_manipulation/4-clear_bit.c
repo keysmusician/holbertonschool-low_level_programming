5c55b1ddb4f8245798b8bfaf84987c58a50f9eb5
keysmusician <justin.masayda@gmail.com>
2021-03-25 20:52:52 +0000

    Add condition for index out of range

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * clear_bit - clears the value of a bit at a given index (sets to 0)
  * @n: binary number
@@ -8,6 +6,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
 	*n &= ~(1 << index);
 	return (1);
 }
