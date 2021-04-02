c487123aa9ba4fa7b6a7fda5168414cb2b1d62f7
keysmusician <justin.masayda@gmail.com>
2021-04-02 15:58:54 +0000

    Revert to original code, add line break for formatting requirement

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,19 +12,33 @@
  * @status_code: I/O function return value;
  * @error_code: code used to determine which error to print
  */
-void error_check(int status_code, int error_code, char *s)
+void error_check(int status_code, int error_code, ...)
 {
+	va_list file;
+
+	if (status_code != 3 && error_code == 97)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(error_code);
+	}
+
 	if (status_code == -1)
 	{
+		va_start(file, error_code);
 		switch (error_code)
 		{
 		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", va_arg(file, char *));
 			break;
 		case 99:
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", va_arg(file, char *));
+			break;
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", va_arg(file, int));
 			break;
 		}
+		va_end(file);
 		exit(error_code);
 	}
 }
@@ -37,40 +51,34 @@ void error_check(int status_code, int error_code, char *s)
  */
 int main(int ac, char **av)
 {
-	int file_from_sts, file_to_sts, read_sts, write_sts;
-	char buffer[BUFFERSIZE];
+	int file_from_sts, file_to_sts, read_sts, close_sts, write_sts;
+	char *file_from, *file_to, buffer[BUFFERSIZE];
 	unsigned int perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	if ((file_from_sts = open(av[1], O_RDONLY)) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if ((file_to_sts = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perms)) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+	error_check(ac, 97);
+
+	file_from = av[1];
+	file_to = av[2];
+
+	file_from_sts = open(file_from, O_RDONLY);
+	error_check(file_from_sts, 98, file_from);
+
+	file_to_sts = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, perms);
+	error_check(file_to_sts, 99, file_to);
+
 	while ((read_sts = read(file_from_sts, buffer, BUFFERSIZE)))
 	{
-		error_check(read_sts, 98, av[1]);
+		error_check(read_sts, 98, file_from);
+
 		write_sts = write(file_to_sts, buffer, read_sts);
-		error_check(write_sts, 99, av[2]);
+		error_check(write_sts, 99, file_to);
 	}
-	if (close(file_from_sts) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_sts);
-		exit(100);
-	}
-	if (close(file_to_sts) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to_sts);
-		exit(100);
-	}
+
+	close_sts = close(file_from_sts);
+	error_check(close_sts, 100, file_from_sts);
+
+	close_sts = close(file_to_sts);
+	error_check(close_sts, 100, file_to_sts);
+
 	return (0);
 }
