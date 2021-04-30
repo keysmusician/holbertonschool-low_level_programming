235d8b653d529fc02da2fd47cc6cefb1741fb76c
keysmusician <justin.masayda@gmail.com>
2021-04-30 15:09:47 +0000

    Fix formatting

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,20 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a doubly linked list
+ * @h: head node of doubly linked list
+ * Return: number of nodes in list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *node_i;
-    size_t nodes_count = 0;
+	const dlistint_t *node_i;
+	size_t nodes_count = 0;
 
-    for (node_i = h; node_i; node_i = node_i->next)
-    {
-        printf("%d\n", node_i->n);
-        nodes_count++;
-    }
+	for (node_i = h; node_i; node_i = node_i->next)
+	{
+		printf("%d\n", node_i->n);
+		nodes_count++;
+	}
 
-    return nodes_count;
+	return (nodes_count);
 }
