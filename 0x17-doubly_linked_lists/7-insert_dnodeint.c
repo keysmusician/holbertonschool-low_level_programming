b263f433385e5fd80aa086cf8b6ca6fe82cea01d
keysmusician <justin.masayda@gmail.com>
2021-05-03 03:18:34 +0000

    Revert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tail = node;
 	}
 
-	if (!node)
+	if (idx > 0)
 		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
