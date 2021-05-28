5d48cf60971ac560942218b0aa07772fbe47661c
keysmusician <justin.masayda@gmail.com>
2021-05-28 16:47:55 +0000

    Revert "Switch malloc() to calloc()"
    This reverts commit 633556be010bba175d737cec62753b11ab535c24.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,13 +13,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!size)
 		return (NULL);
 
-	hash_table = calloc(1, sizeof(hash_table_t));
+	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
 
 	hash_table->size = size;
 
-	hash_node_array = calloc(size, sizeof(hash_node_t *));
+	hash_node_array = malloc(sizeof(hash_node_t *) * size);
 	if (!hash_node_array)
 	{
 		free(hash_table);
